fda4456ba15bb97ebba1b8f7901b2fc9a609471f
Sumukesh <sumukeshmopuram1@gmail.com>
2025-12-10 21:18:52 +0530

    changed code for deployement

--- frontend/src/components/Auth/ForgotPassword.js
+++ frontend/src/components/Auth/ForgotPassword.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import axios from 'axios';
-const API_URL =  "http://localhost:3001" || process.env.REACT_APP_API_URL ;
+const API_URL =   process.env.REACT_APP_API_URL || "http://localhost:3001" ;
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
